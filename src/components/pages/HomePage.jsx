d4b3c2c11b596c3cad2d6ea67272d43071020ad2
aeast18 <32113600+aeast18@users.noreply.github.com>
2019-03-16 22:12:55 -0400

    initial changes to homepage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -16,54 +16,31 @@ import {
     Button
 } from 'framework7-react';
 
+var styles_col = {
+  margin: '20px',
+  width: '200px',
+  height: '200px',
+  backgroundColor: 'blue',
+  display: 'inline-block',
+};
+
 export default () => (
   <Page>
     <Navbar>
-      <NavLeft>
-        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="left"></Link>
-      </NavLeft>
-      <NavTitle>My App</NavTitle>
-      <NavRight>
-        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right"></Link>
-      </NavRight>
-    </Navbar>
-    <Toolbar bottom>
-      <Link>Left Link</Link>
-      <Link>Right Link</Link>
+      <NavTitle>Karbon Killer</NavTitle>
+     </Navbar>
+     <Toolbar bottom>
+      <Link>Transportation</Link>
+      <Link> Residence </Link>
+      <Link>Diet</Link>
     </Toolbar>
-    <Block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </Block>
-    <BlockTitle>Navigation</BlockTitle>
-    <List>
-      <ListItem link="/about/" title="About"></ListItem>
-      <ListItem link="/form/" title="Form"></ListItem>
-    </List>
-    <BlockTitle>Modals</BlockTitle>
-    <Block strong>
-      <Row>
-        <Col width="50">
-          <Button fill raised popupOpen="#popup">Popup</Button>
-        </Col>
-        <Col width="50">
-          <Button fill raised loginScreenOpen="#login-screen">Login Screen</Button>
-        </Col>
-      </Row>
-    </Block>
-    <BlockTitle>Panels</BlockTitle>
-    <Block strong>
-      <Row>
-        <Col width="50">
-          <Button fill raised panelOpen="left">Left Panel</Button>
-        </Col>
-        <Col width="50">
-          <Button fill raised panelOpen="right">Right Panel</Button>
-        </Col>
-      </Row>
-    </Block>
-    <List>
-      <ListItem link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></ListItem>
-      <ListItem link="/load-something-that-doesnt-exist/" title="Default Route (404)"></ListItem>
-    </List>
-  </Page>
+    <body>
+    <div class="row">
+  <div class="column"> col 1</div>
+  <div class="column">col 2</div>
+  <div class="column">col 3</div>
+</div> 
+    </body>
+      
+</Page>   
 );
